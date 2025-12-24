0daa035dd36c6de0fca5bf0a190a36aeb640c5d8
KAMUSODHABARWMR <167154881+KAMUSODHABARWMR@users.noreply.github.com>
2025-12-24 10:23:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # config.py
 import os
 BOT_NAME = os.environ.get('BOT_NAME')
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-API_ID = int(os.environ.get('API_ID'))
-API_HASH = os.environ.get('API_HASH')
+BOT_TOKEN = os.environ.get('8408673218:AAF7ynuN-t92qdHp7N2fMpf_0NUSDxNLRnM')
+API_ID = int(os.environ.get('21442913'))
+API_HASH = os.environ.get('b95b8bae367e19c3356b9f0004b218c2')
 MONGO_URI = os.environ.get('MONGO_URI')
+
